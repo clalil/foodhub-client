1d28c55a29c8d5adad8a2d6ba81c69fc56c23ea1
Becca <beccaburns89@gmail.com>
2019-11-21 13:40:52 +0100

    Writes content for about us component for Create, Fork, & Save.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Divider, Grid, Icon, List, Segment, Button, Step, Modal } from 'semantic-ui-react'
+import { Container, Divider, Grid, Icon, Image, Header, Segment, Button, Step, Modal } from 'semantic-ui-react'
 import ForkButton from '../img/ForkButton.png'
 import '../css/about-us.css'
 
@@ -26,23 +26,53 @@ const AboutUs = () => {
           <Step.Group widths={3}>
             <Step>
               <Icon name='food' />
-              <Modal trigger={<Button><Step.Content>
+              <Modal trigger={<Button basic><Step.Content>
                 <Step.Title>Create</Step.Title>
               </Step.Content></Button>}>
-              
+              <Modal.Header>Create Recipes</Modal.Header>
+                <Modal.Content image>
+                  <Image wrapped size='large' src='https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/3329/7f164063ce0ede8959b2/letterbox_SMALLERSharingBread_103918.jpg' />
+                  <Modal.Description>
+                    <h3>Once you create an account on FOODHUB, you can create your own recipe!</h3>
+                    <h3>What fun recipes would you like to share with our FOODHUB community of foodies?</h3>
+                    <p>Create an account today!</p>
+                  </Modal.Description>
+                </Modal.Content>
               </Modal>
             </Step>
             <Step>
               <Icon name='plus' />
-              <Step.Content>
+              <Modal trigger={<Button basic><Step.Content>
                 <Step.Title>Fork</Step.Title>
-              </Step.Content>
+              </Step.Content></Button>}>
+              <Modal.Header>Fork & Customize Recipes</Modal.Header>
+              <Modal.Content image>
+                  <Image wrapped size='medium' src='https://www.forkintheroad.co/wp-content/uploads/2018/09/pan-roasted-okra-107.jpg' />
+                  <Modal.Description>
+                    <h3>You've tried a recipe on FOODHUB. Now, you want to add it to your collection and add another ingredient for next time.</h3>
+                    <h2>No Problem!</h2>
+                    <h3>When you Fork other Foodies recipes by clicking the "fork" button on their recipe, you can customize it to make it your own.</h3>
+                    <h3>Your connections will now see all of the recipes you forked and the user's you forked them from.</h3>
+                  </Modal.Description>
+                </Modal.Content>
+              </Modal>
             </Step>
             <Step>
-              <Icon name='group' />
-              <Step.Content>
-                <Step.Title>Share</Step.Title>
-              </Step.Content>
+              <Icon name='book' />
+              <Modal trigger={<Button basic><Step.Content>
+                <Step.Title>Save</Step.Title>
+              </Step.Content></Button>}>
+              <Modal.Header>Collect your Favorite Recipes in a Cookbook</Modal.Header>
+              <Modal.Content image>
+                  <Image wrapped size='large' src='https://www.peacefuldumpling.com/wp-content/uploads/2017/03/moon-juice-cookbook-3-700x467.jpg' />
+                  <Modal.Description>
+                    <h3>Made with love by FOODHUB.</h3>
+                    <h3>Easily make a cookbook at FOODHUB by adding your favorite recipes to your own Cookbook created by us.
+                        Access your recipes from anywhere online and even better, get your own PDF!
+                    </h3>
+                  </Modal.Description>
+                </Modal.Content>
+              </Modal>
             </Step>
           </Step.Group>
         
