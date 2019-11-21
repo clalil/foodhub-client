3c241f8a55d0e75af6939c8ba9d764cfcf39b52e
clalil <cliljander@gmail.com>
2019-11-21 17:16:38 +0100

    Add initial feature test

--- cypress/integration/userCanViewSingleRecipe.spec.js
+++ cypress/integration/userCanViewSingleRecipe.spec.js
@@ -52,7 +52,29 @@ describe('View single recipe', () => {
       cy.get('.header').should('contain', 'Quiche')
         .get('[name="recipe-ingredients"]').should('contain', 'Eggs')
         .get('[name="recipe-directions"]').should('contain', 'Stir the mixture')
+    })  
+  })
+
+  it('A recipe can have ratings', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3000/v1/recipes/2',
+      response: 'fixture:single_recipe.json',
+      status: 200
+    }),
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/v1/recipes/1',
+      response: '{"message": "You like this recipe!"}',
+      status: 201
     })
-    
+
+    cy.loginUser('user@mail.com', 'password')
+    cy.get('#recipe-1').click({ force: true })
+    cy.get('#likes').should('contain', '0')
+    cy.get('name["like-btn"]').click()
+    cy.get('#response-message')
+      .should('contain', 'You like this recipe!')
+    cy.get('#likes').should('contain', '1')
   })
 })
